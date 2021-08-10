955dd29c8a962c00ede8330496c6432f94050db9
lsandoval9 <lizarraga309@gmail.com>
2021-08-10 18:45:57 -0400

    added mainnet forking through alchemyapi

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,15 @@
 import "@nomiclabs/hardhat-waffle";
 
-import { task } from "hardhat/config"
+import { task } from "hardhat/config";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+    const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+    for (const account of accounts) {
+        console.log(account.address);
+    }
 });
 
 // You need to export an object to set up your config
@@ -19,5 +19,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.6",
+    solidity: {
+        version: "0.8.6",
+    },
+    networks: {
+        hardhat: {
+            forking: {
+                url: "https://eth-mainnet.alchemyapi.io/v2/CyI3yTBWSg1tBk5cCWEPIuZw5InGryfY",
+            },
+        },
+    },
 };

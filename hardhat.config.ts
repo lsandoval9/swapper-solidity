aab16bfee44cc72cd2d8cf6074c2ae8cf5e236a7
lsandoval9 <lizarraga309@gmail.com>
2021-08-10 14:51:16 -0400

    added README.md file and changed project's name to swapper-eyss

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
 import "@nomiclabs/hardhat-waffle";
 
-import { task} from "hardhat/config"
+import { task } from "hardhat/config"
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
